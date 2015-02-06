b08f23bf06746caad1886c68462df3cef46e99a3
Greg Newbrough <gnewbrough@walzgroup.com>
2015-02-06 09:24:01 -0800

    init nancy

---
+++ NancyDemo/NancyDemo/Modules/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace NancyDemo.Modules
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
